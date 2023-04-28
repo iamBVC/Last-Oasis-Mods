49712d35158a6a192f5b836465dbbf5042ff7182
iamBVC <briancastellese@gmail.com>
2023-04-28 20:24:31 +0200

    faster script

--- Tools/pack_all_pmods.bat
+++ Tools/pack_all_pmods.bat
@@ -14,14 +14,4 @@ del /q "%client_path%\pmod\template.sig"
 )
 
 del /q "%server_path%\pmod"
-
-for /d %%i in ("pmod_*") do (
-
-@echo "..\%%i\*.*" "..\..\..\*.*" >.\pack\filelist.txt
-.\pack\UnrealPak.exe "%server_path%\pmod\%%i.pak" -create="filelist.txt" -compress
-
-mkdir "%server_path%\pmod\"
-XCOPY /S /Y ".\pack\template.sig" "%server_path%\pmod\"
-ren "%server_path%\pmod\template.sig" "%%i.sig"
-del /q "%server_path%\pmod\template.sig"
-)
+XCOPY /S /Y "%client_path%\pmod\" "%server_path%\pmod\"
