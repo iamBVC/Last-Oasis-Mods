968d5bca83b2ebfe2cfff94f252c127a33dabdbb
iamBVC <briancastellese@gmail.com>
2023-05-21 14:27:45 +0200

    asset registry merger

---
+++ Tools/AssetRegistryHelper/merge.bat
@@ -0,0 +1,1 @@
+AssetRegistryHelper "..\Last_Oasis_Client\Mist\AssetRegistry.bin" -Merge "D:\Documenti\Unreal Projects\Mist\Saved\Cooked\WindowsNoEditor\Mist\AssetRegistry.bin" "..\pmod_Configs\Mist\AssetRegistry.bin"
