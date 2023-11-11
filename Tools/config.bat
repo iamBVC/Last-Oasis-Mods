03577de629e9c3cdec40e8429607051cd51ccc9b
iamBVC <briancastellese@gmail.com>
2023-11-11 15:01:33 +0100

    update

--- Tools/config.bat
+++ Tools/config.bat
@@ -1,5 +1,5 @@
-set server_path=F:\Programmi\Steam\steamapps\common\Last Oasis - Dedicated Server\Mist\Content\Paks
-set client_path=F:\Programmi\Steam\steamapps\common\Last Oasis\Mist\Content\Paks
-set server_git_path=F:\Giochi\Last Oasis\Modding\loc.iambvc.it\server\MODS
-set client_git_path=F:\Giochi\Last Oasis\Modding\loc.iambvc.it\client\Mist\Content\Paks
-set cooked_path=F:\Unreal Projects\Mist\Saved\Cooked\WindowsNoEditor
+set server_path=G:\Programmi\Steam\steamapps\common\Last Oasis - Dedicated Server\Mist\Content\Paks
+set client_path=G:\Programmi\Steam\steamapps\common\Last Oasis\Mist\Content\Paks
+set server_git_path=G:\Giochi\Last Oasis\Modding\loc.iambvc.it\server\MODS
+set client_git_path=G:\Giochi\Last Oasis\Modding\loc.iambvc.it\client\Mist\Content\Paks
+set cooked_path=G:\Unreal Projects\Mist\Saved\Cooked\WindowsNoEditor
